8891775e424a278c7c8e0f8d9cace5613a67944e
sparta15 <guillermo.pastordiez@telefonica.com>
2019-05-11 00:21:56 +0200

    chore: change confirmate proposal style

--- src/components/ConfirmateProposal.vue
+++ src/components/ConfirmateProposal.vue
@@ -1,9 +1,32 @@
 <template>
   <div>
     <div class="container mt-5">
-      <h3>Propuesta Enviada</h3>
-      <img src="chack-green.png" alt="White Check">
-      <button type="button" class="btn btn-primary" v-on:click="Accept()">Aceptar</button>
+      <div class="row">
+        <div class="col align-self-center">
+          <h3 class="text-center">Propuesta Enviada</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4"></div>
+        <div class="col-4">
+          <h5>Tu propuesta ha sido enviada y pronto estará disponible.</h5>
+        </div>
+        <div class="col-4"></div>
+      </div>
+      <div class="row">
+        <div class="col-5"></div>
+        <div class="col-2">
+          <img src="../assets/check-green.png">
+        </div>
+        <div class="col-5"></div>
+      </div>
+      <div class="row">
+        <div class="col-5"></div>
+        <div class="col-2">
+          <button type="button" class="btn btn-primary btn-lg" v-on:click="Accept()">Aceptar</button>
+        </div>
+        <div class="col-5"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -18,3 +41,10 @@ export default {
   }
 }
 </script>
+
+<style>
+  img {
+    width: 75%;
+    height: 75%;
+  }
+</style>
