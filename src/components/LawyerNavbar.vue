eee3e83a5e333f6192270284a7eee7d38892d349
sparta15 <guillermo.pastordiez@telefonica.com>
2019-05-11 10:34:52 +0200

    feat: access document

--- src/components/LawyerNavbar.vue
+++ src/components/LawyerNavbar.vue
@@ -4,6 +4,7 @@
     <form class="form-inline">
         <router-link :to="{path:'/perfilabogado'}"><button class="btn btn-outline-success m-2" type="button">Mi Perfil</button></router-link>
         <router-link :to="{path:'/propuestas'}"><button class="btn btn-outline-success m-2" type="button">Propuestas</button></router-link>
+        <router-link :to="{path:'/documento'}"><button class="btn btn-outline-success m-2" type="button">Estatuto de la abogacía</button></router-link>
         <router-link :to="{path:'/codigoconducta'}"><button class="btn btn-outline-success m-2" type="button">Código de conducta</button></router-link>
         <button type="submit" class="btn btn-secondary m-2" v-on:click="logout()">Cerrar sesión</button>
     </form>
