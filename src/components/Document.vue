95906ff99dc2f451593b63e668d217890fa082c9
sparta15 <guillermo.pastordiez@telefonica.com>
2019-05-11 10:40:07 +0200

    chore: feedback in document

--- src/components/Document.vue
+++ src/components/Document.vue
@@ -24,7 +24,7 @@
           <div class="card-body">
             <h5 class="card-title">Segundo artículo</h5>
             <p class="card-text">La tutela judicial efectiva se garantizará a aquellas personas cuyos ingresos anuales no superen los ocho mil euros.</p>
-            <button type="button" class="btn btn-primary" v-on:click="ReviewArticle()">Revisar</button>
+            <h6>Propuesta definitiva</h6>
           </div>
         </div>
       </div>
@@ -37,6 +37,9 @@
             <h5 class="card-title">Tercer artículo</h5>
             <p class="card-text">Todo ciudadano tendrá derecho a la defensa, según lo establecido en nuestra Carta Magna. Esto se conseguirá mediante el derecho a una tutela judicial efectiva, quien podrá optar a ello si cumple con los requisitos establecidos en la ley.</p>
             <button type="button" class="btn btn-primary" v-on:click="ReviewArticle()">Revisar</button>
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 25%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
           </div>
         </div>
       </div>
